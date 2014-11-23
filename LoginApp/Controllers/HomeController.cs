0f2198a791be2bb0729cb5deebe294bbe33527a9
Uditha <udithapriyasanka4@gmail.cm>
2014-11-23 14:32:41 +0530

    Hello Commit Comment

---
+++ LoginApp/Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoginApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Login(loginTable u)
+        {
+            if (ModelState.IsValid)
+            {
+                using (LoginDatabaseEntities dc = new LoginDatabaseEntities())
+                {
+                    var v = dc.loginTables.Where(a => a.userName.Equals(u.userName) && a.password.Equals(u.password)).FirstOrDefault();
+                    if (v != null)
+                    {
+                        Session["LogedUserId"] = v.userId.ToString();
+                        Session["LogedUserName"] = v.userName.ToString();
+                        return RedirectToAction("AfterLogin");
+                    }
+                }
+            }
+            return View(u);
+        }
+
+        public ActionResult AfterLogin()
+        {
+            if (Session["LogedUserId"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+            
+        }
+    }
+}
---
+++ LoginApp/Database/dbo.Table.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[userId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [userName] VARCHAR(100) NOT NULL, 
+    [password] VARCHAR(25) NOT NULL
+)
